d1d4bcf7df55f63910459e14127433d8f1e3f288
samlcallahan <callahan.l.sam@gmail.com>
2021-11-02 11:04:47 -0500

    Wrote basic structure

---
+++ random_album.py
@@ -0,0 +1,11 @@
+import spotify
+from env import user
+import random
+
+def get_albums(user):
+    return albums
+
+def random_album(user):
+    albums = get_albums(user)
+    album = random.choice(albums)
+    return album_title
